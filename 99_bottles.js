643af9f2d20fd591b023dc34b70e861217f1296d
Miah Clay <miahb88@gmail.com>
2022-05-23 20:46:24 -0400

    Answered the bottle song

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,13 @@
 function bottleSong() {
   // Write your code here!
+  for (let i=99; i>2; i--) {
+    let answer = ""
+    answer += i
+    console.log(answer + " bottles of beer on the wall, " + answer + " bottles of beer. Take one down and pass it around, " + (answer - 1) + " bottles of beer on the wall.")
+  }
+
+  console.log("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.")
+  
 };
 
 bottleSong();
