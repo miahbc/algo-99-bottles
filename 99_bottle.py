24749713dc093ee5dca165c8a7496dce7e30829d
Miah Clay <miahb88@gmail.com>
2022-05-25 14:09:24 -0400

    python version done

---
+++ 99_bottle.py
@@ -0,0 +1,17 @@
+def bottle_song():
+    x = 99
+    while (x > 2):
+        print(f"{x} bottles of beer on the wall! {x} bottles of beer! Take one down, pass it around, {x-1} bottles of beer on the wall!")
+        x = x - 1
+    print("2 bottles of beer on the wall! 2 bottles of beer! Take one down, pass it around, 1 bottle of beer on the wall.")
+  
+    # }
+#   // Write your code here!
+#   for (let i=99; i>2; i--) {
+#     let answer = ""
+#     answer += i
+#     console.log(answer + " bottles of beer on the wall, " + answer + " bottles of beer. Take one down and pass it around, " + (answer - 1) + " bottles of beer on the wall.")
+#   }
+  
+
+bottle_song();
